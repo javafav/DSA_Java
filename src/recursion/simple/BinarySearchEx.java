38365647f3ed8aef27779f916956e688e70d6d26
sanykhan <usmanshj3@gmail.com>
2024-12-19 14:49:44 +0500

    Decimal to binary conversion

--- src/recursion/simple/BinarySearchEx.java
+++ src/recursion/simple/BinarySearchEx.java
@@ -1,9 +1,9 @@
 package src.recursion.simple;
 
-public class BinarySearch {
+public class BinarySearchEx {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 5, 9, 11, 47};
-        int target = 48 ;
+        int target = 11;
         int foundNumber = findNumber(numbers, target, 0, numbers.length - 1);
         if (foundNumber > 0) {
             System.out.println(target + " Number found at index " + foundNumber);
@@ -15,18 +15,17 @@ public class BinarySearch {
     }
 
     static int findNumber(int[] numbers, int target, int left, int right) {
-        if (left > right) {
-            return -1;
-        }
-        int mid = left + right  / 2;
-        if (target == numbers[mid]) {
-            return mid;
-        }else if(target< numbers[mid]){
-            return findNumber(numbers, target, left, mid - 1);
-        }else {
-            return findNumber(numbers, target, mid + 1, right);
-        }
-
+     if(right < left){
+         return -1;
+     }
+     int mid = (left + right) / 2;
+     if(numbers[mid] == target){
+         return target;
+     }else if(target  < numbers[mid]){
+         return findNumber(numbers, target, left, mid - 1);
+     }else {
+         return findNumber(numbers, target, mid + 1, right);
+     }
     }
 
 }
